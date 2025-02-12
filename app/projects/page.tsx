a85691a8155b139e7341b1c6a201e4805a7ebb13
unknown <amaaliousama771@gmail.com>
2025-02-12 22:58:50 +0100

    fin

---
+++ app/projects/page.tsx
@@ -0,0 +1,66 @@
+
+
+import Link from "next/link";
+import React from "react";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+import { Article } from "./article";
+import { Eye } from "lucide-react";
+
+export const revalidate = 60;
+
+export default function ProjectsPage() {
+  // Parse the JSON data from the environment variable
+  const projects: Project[] = JSON.parse(process.env.NEXT_PUBLIC_PROJECTS || "[]");
+
+
+  return (
+    <div className="relative pb-16">
+      <Navigation />
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Projects
+          </h2>
+       
+        </div>
+        <div className="w-full h-px bg-zinc-800" />
+
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2">
+          {/* Loop over the first two projects */}
+          {projects.slice(0, 5).map((project, index) => (
+            <Card key={index}>
+              <Link href={project.href}>
+                <article className="relative w-full h-full p-4 md:p-8">
+                  <div className="flex items-center justify-between gap-2">
+                    <div className="text-xs text-zinc-100">
+                      <span>{project.name}</span>
+                    </div>
+                    <span className="flex items-center gap-1 text-xs text-zinc-500">
+                      <Eye className="w-4 h-4" />
+                    </span>
+                  </div>
+
+                  <h2
+                    id="featured-post"
+                    className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                  >
+                    {project.name}
+                  </h2>
+                  <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                    {project.description}
+                  </p>
+                  <div className="absolute bottom-4 md:bottom-8">
+                 
+                  </div>
+                </article>
+              </Link>
+            </Card>
+          ))}
+        </div>
+
+       
+      </div>
+    </div>
+  );
+}
